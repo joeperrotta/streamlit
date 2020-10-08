32729d7d60e1faf448627450e8b7151e9faeb67d
joeperrotta <56735310+joeperrotta@users.noreply.github.com>
2020-10-08 10:29:03 -0400

    Update JP_test.py

--- JP_test.py
+++ JP_test.py
@@ -33,6 +33,7 @@ from PIL import Image
 image = Image.open('cover.jpg')
 matplotlib.use("Agg")
 st.set_option('deprecation.showfileUploaderEncoding', False)
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
 class DataFrame_Loader():
